5fff311ea6f22b364a1d5bc724e55efe209b2f3f
2ld <364605397@qq.com>
2020-08-27 12:57:17 +0800

    refactor 调整项目结构

---
+++ warehourse/src/page/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-container>
+    <el-header class="page-header">湘湘仓储管理系统 </el-header>
+    <el-main
+      ><el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-menu-item index="3" disabled>我不能点</el-menu-item>
+      </el-menu>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1',
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.page-header {
+  color: aquamarine;
+  .hahah {
+    color: red;
+  }
+}
+</style>
--- warehourse/src/router/index.js
+++ warehourse/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import index from '../page/index'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: index
     }
   ]
 })
